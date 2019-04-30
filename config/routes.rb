e24bc4e59ffeeb7270c6dd7bca2335d033c1c593
“a-liudezhi” <think72013@aliyun.com>
2019-05-01 00:02:38 +0800

    replace root with groups#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
    resources :groups 
-   root 'welcome#index'
+   root 'groups#index'
 
 end
