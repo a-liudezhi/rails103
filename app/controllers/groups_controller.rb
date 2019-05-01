d980790d35dd0b68f3b72aa1cfc3c4bc264bb58a
“a-liudezhi” <think72013@aliyun.com>
2019-05-01 11:25:48 +0800

    implement groups#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,8 +3,19 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
   def new
-    @group = Group.new 
+    @group = Group.new
   end
 
+  def create
+    @group = Group.new(group_params)
+    @group.save
 
+    redirect_to groups_path
+  end
+  
+private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
