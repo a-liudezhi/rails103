7a4cbfbc0e9cdf8d1f0ce67e41c70fea518d880f
“a-liudezhi” <think72013@aliyun.com>
2019-05-01 11:49:57 +0800

    implement groups#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,11 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+  
   def new
     @group = Group.new
   end
@@ -12,7 +17,7 @@ class GroupsController < ApplicationController
 
     redirect_to groups_path
   end
-  
+
 private
 
   def group_params
