1eac1522d3960206887ac1575c940725e3926dfa
“a-liudezhi” <think72013@aliyun.com>
2019-05-02 19:35:24 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
- before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+ before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
  before_action :find_group_and_check_permisson, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
@@ -7,7 +7,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
-    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5) 
+    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
   end
 
   def edit
@@ -41,6 +41,30 @@ end
     @group.destroy
     flash[:alert] = "Group deleted"
     redirect_to groups_path
+  end
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本讨论群成功！"
+    else
+      flash[:warning] = "你已经是本讨论群成员了！"
+  end
+
+  redirect_to group_path(@group)
+end
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本讨论群了！"
+    else
+      flash[:warning] = "你不是本讨论群成员，怎么退出   XD"
+    end
+
+    redirect_to group_path(@group)
   end
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
    resources :groups do
+     member do
+       post :join
+       post :quit
+     end
      resources :posts
    end
    root 'groups#index'
